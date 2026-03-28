6fbaa13e12aba2bd14ace3b0f3e1b1063547df1a
Mehdi Shayek <mehdishayek@gmail.com>
2026-03-29 04:38:23 +0530

    Revert "Add Docker setup + Redis adapter + local cron scheduler"
    This reverts commit 1e1b87099f8134b254218293e3141493c9b157c8.

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
   experimental: {
-    instrumentationHook: true,
     serverComponentsExternalPackages: ['pdf-parse'],
     serverActions: {
       bodySizeLimit: '10mb',
